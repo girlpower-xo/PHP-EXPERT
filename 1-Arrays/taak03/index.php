14de54e188cb7057c5e4736d696edefd99b9c042
Iris Petersen <iris.petersen@student.rocva.nl>
2020-09-03 12:05:24 +0200

    3 done

---
+++ 1-Arrays/taak03/index.php
@@ -0,0 +1,39 @@
+<?php
+$medewerkers = [
+    [
+      'voornaam' => 'Willem',
+      'achternaam' => 'van Oranje',
+      'functie'  => 'koning'
+    ],
+    [
+      'voornaam' => 'Donald',
+      'achternaam' => 'Trump',
+      'functie'  => 'president'
+    ],
+    [
+      'voornaam' => 'Homer',
+      'achternaam' => 'Simpson',
+      'functie'  => 'fabrieksarbeider'
+    ],
+    [
+      'voornaam' => 'Johan',
+      'achternaam' => 'Cruyff',
+      'functie'  => 'voetbalicoon'
+    ]
+    ,
+    [
+      'voornaam' => 'Badr',
+      'achternaam' => 'Hari',
+      'functie'  => 'Kickboxer'
+    ]
+  ];
+  
+ 
+  
+
+
+
+foreach($medewerkers as $medewerker){
+    echo $medewerker['voornaam']. $medewerker['achternaam'] . $medewerker['functie'] . '<br>';
+    
+}
