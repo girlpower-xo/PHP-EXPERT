3d093c3c284d3a5e5888b4d88c9b13cdda466fc2
Iris Petersen <iris.petersen@student.rocva.nl>
2020-09-07 12:50:34 +0200

    4 done

--- 1-Arrays/taak04/index.php
+++ 1-Arrays/taak04/index.php
@@ -20,6 +20,8 @@ $speelgoed = [
 ];
 
 
+
+
 $games = [
     'sport' => [
         'naam' => 'Fifa',
@@ -35,5 +37,16 @@ $games = [
         'naam' => 'Left for dead 2',
         'uitgever' => 'Valve',
         'prijs' => '40.00'
-    ]
+        ]
 ];
+
+//echo "speelgoed " ." ". $speelgoed[0]['naam'] ." ". "uit de catogorie" ." ".  $speelgoed[0]['soort'] ." ".  "kost" ." ". $speelgoed[0]['prijs'];
+// het kan op deze manier maar dan moet je heel veel uit type het kan makkelijker met een foreach loop zie hier onder
+
+foreach ($speelgoed as $speel => $value) {
+    echo $value['naam'] . " uit de categorie " . $value['soort'] . ' kost ' . $value['prijs'] . '<br>';
+}
+
+foreach ($games as $game => $value) {
+    echo $value['naam'] . " uit de categorie " . $value['uitgever'] . ' kost ' . $value['prijs'] . '<br>';
+}
