289ed90ccde2864b4b0409c710af4f956daf3fa2
Iris Petersen <iris.petersen@student.rocva.nl>
2020-12-10 12:58:29 +0100

    1 done

--- 3-PDO-SELECT/taak01/index.php
+++ 3-PDO-SELECT/taak01/index.php
@@ -1,20 +1,16 @@
 <?php 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-$database_lokatie     = 'localhost';
-$database_naam        = 'toolsforever';
-$database_gebruiker   = 'root';
-$database_wachtwoord  = '';
+$mysqli = new mysqli('localhost', 'root', '', 'toolsforever');
+if ($mysqli->connect_error) {
+    die('');
+}
 
-$database_connectie = new PDO("mysql:host=$database_lokatie;dbname=$database_naam", $database_gebruiker, $database_wachtwoord);
-
-
-$sql = "SELECT * FROM users";
-$stmt = $db_conn->prepare($sql);
-$stmt ->execute();
-$database_gegevens = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-$sql = "SELECT kolommen FROM table WHERE kolom = :placeholder";
-$stmt = $db_conn->prepare($sql);
-$stmt ->bindParam(":placeholder", $placeholder_variabele);
-$stmt ->execute();
-$database_gegevens = $statement->fetch(PDO::FETCH_ASSOC);
+$stmt = $mysqli->query('SELECT * FROM users');
+    foreach ($stmt as $row){
+        echo $row["firstname"];
+    }
+                
+?>
